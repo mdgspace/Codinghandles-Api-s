9f9a7ccc969b8b19d007e95fb572dd194637081f
yash <yp969803@gmail.com>
2024-03-26 16:05:15 +0530

    feat: cors

--- app.py
+++ app.py
@@ -3,6 +3,8 @@ from routes import routes
 import jinja2
 import aiohttp_jinja2
 import  os
+from aiohttp_cors import CorsConfig, setup
+
 
 app = web.Application() 
 
@@ -10,6 +12,13 @@ aiohttp_jinja2.setup(
     app, loader=jinja2.FileSystemLoader(os.path.join(os.getcwd(), "templates"))
 )
 
+cors = CorsConfig(
+        allow_all_origins=True,  # Allow all origins
+        allow_credentials=True,  # Allow credentials
+        allow_all_methods=True,  # Allow all HTTP methods
+        allow_all_headers=True   # Allow all headers
+       )
+setup(app, defaults=cors)
 router = app.router
 
 routes.setup_routes(router)
